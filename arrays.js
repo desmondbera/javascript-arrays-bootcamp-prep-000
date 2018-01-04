6ed713bfd7aaf6a575afe1c2f73e17149537331e
desmond beramendi <desmond7187@gmail.com>
2018-01-04 19:10:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -38,6 +38,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  array.slice(0, array.length - 1);
-  return array;
+  new_arr = array.slice(0, array.length - 1);
+  return new_arr;
 }
